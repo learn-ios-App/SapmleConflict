ae47ef07b50add67e5a3cfe0b19d4bea587c48da
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-03 22:21:47 +0900

    Initial Commit

---
+++ SapmleConflict/SapmleConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  SapmleConflictApp.swift
+//  SapmleConflict
+//
+//  Created by 渡邊魁優 on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct SapmleConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
